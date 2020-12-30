d73909c32e6d7dfb084f49de025cc9d58a4275fb
huzl <719813426@qq.com>
2020-12-30 11:57:59 +0800

    更新

--- activityServiceImpl.java
+++ activityServiceImpl.java
@@ -9,3 +9,24 @@ import org.springframework.transaction.annotation.Transactional;
 
 
 
+@Service
+@Transactional(readOnly=false)
+public class TestServiceImpl {
+
+
+@Resource
+private TestDaoImpl testDaoImpl;
+
+
+public void regist(){
+this.testDaoImpl.saveUser();
+}
+
+
+public void login(){
+this.testDaoImpl.login();
+}
+
+
+
+}
