9b5399e33b54c28110b25d1d4ee3eef8b8ea18d4
huzl <719813426@qq.com>
2020-12-30 11:43:35 +0800

    创建activityuserviceImpl文件

---
+++ activityServiceImpl.java
@@ -0,0 +1,11 @@
+package com.bear.cakeonline.test;
+
+
+import javax.annotation.Resource;
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+
